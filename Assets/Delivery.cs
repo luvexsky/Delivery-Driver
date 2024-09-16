00737e547b962656033d3eb03c92f07601223b19
luvexsky <ngryemo@gmail.com>
2024-09-16 12:04:49 -0400

    Finish

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -6,7 +6,18 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
+  [SerializeField] Color32 hasPackageColor = new Color32 (1, 1, 1, 1);
+  [SerializeField] Color32 noPackageColor = new Color32 (1, 1, 1, 1);
+   
+   [SerializeField] float destroyDelay = 0.5f;
    bool hasPackage;
+
+   SpriteRenderer spriteRenderer;
+
+   void Start()
+   {
+    spriteRenderer = GetComponent<SpriteRenderer>();
+   }
  void OnCollisionEnter2D(Collision2D other)
  {
     Debug.Log("Ouch");
@@ -18,12 +29,14 @@ public class Delivery : MonoBehaviour
     {
       Debug.Log("Package picked up");
       hasPackage = true;
+      spriteRenderer.color = hasPackageColor;
       Destroy(other.gameObject, 0.5f);
     }
     if (other.tag == "Customer" && hasPackage) 
     {
       Debug.Log("Package delivered");
       hasPackage =  false;
+      spriteRenderer.color = noPackageColor;
     }
  }
 }
--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -1,17 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
    [SerializeField] float steerSpeed = 0.5f;
    [SerializeField] float moveSpeed = 0.01f;
+   [SerializeField] float slowSpeed = 0.1f;
+   [SerializeField] float boostSpeed = 35f;
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
@@ -19,5 +17,18 @@ public class NewBehaviourScript : MonoBehaviour
         float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
         transform.Rotate(0, 0, -steerAmount);
         transform.Translate(0, moveAmount, 0);
+    }
+
+void OnCollisionEnter2D(Collision2D other)
+{
+    moveSpeed = slowSpeed;
+
+}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Boost")
+        {
+            moveSpeed = boostSpeed;
+        }
     }
 }
