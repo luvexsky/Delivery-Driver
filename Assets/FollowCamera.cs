e8531c2c79ff3d55054cf5e5ba84ab45926d26f7
luvexsky <ngryemo@gmail.com>
2024-09-16 00:47:47 -0400

    Update FollowCamera.cs

--- Assets/FollowCamera.cs
+++ Assets/FollowCamera.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowCamera : MonoBehaviour
 {
     [SerializeField] GameObject thingToFollow;
-    void Update()
+    void LateUpdate()
     {
        transform.position = thingToFollow.transform.position + new Vector3 (0, 0, -10);
     }
