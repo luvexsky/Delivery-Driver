ac78948f66c876d712418ec92809b29b6717964f
luvexsky <ngryemo@gmail.com>
2024-09-16 11:40:38 -0400

    fixed bool

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -14,10 +14,11 @@ public class Delivery : MonoBehaviour
 
  void OnTriggerEnter2D(Collider2D other)
  {
-    if (other.tag == "Package")
+    if (other.tag == "Package" && !hasPackage)
     {
       Debug.Log("Package picked up");
       hasPackage = true;
+      Destroy(other.gameObject, 0.5f);
     }
     if (other.tag == "Customer" && hasPackage) 
     {
