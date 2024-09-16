0328777546b211b973b05add9168b9cedfbc4077
luvexsky <ngryemo@gmail.com>
2024-09-16 00:46:57 -0400

    Assets and more

---
+++ Assets/Collision.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Collision : MonoBehaviour
+{
+ void OnCollisionEnter2D(Collision2D other)
+ {
+    Debug.Log("Ouch");
+ }
+
+ void OnTriggerEnter2D(Collider2D other)
+ {
+    Debug.Log("What was that?!");
+ }
+}
