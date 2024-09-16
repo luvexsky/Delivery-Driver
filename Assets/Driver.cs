f65cf6d0e820ebbf8323740b68cbd4f8c4680111
luvexsky <ngryemo@gmail.com>
2024-09-15 22:14:19 -0400

    Controls

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-   [SerializeField] float steerSpeed = 0.1f;
+   [SerializeField] float steerSpeed = 0.5f;
    [SerializeField] float moveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
@@ -15,7 +15,9 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Rotate(0, 0,steerSpeed);
-        transform.Translate(0, moveSpeed, 0);
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed;
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
